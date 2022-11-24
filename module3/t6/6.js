c592395c4946fbca2faf10b82290e133bdd66b64
kurban3786 <111856798+kurban3786@users.noreply.github.com>
2022-11-25 00:40:04 +0200

    Delete module3 directory

--- module3/t6/6.js
+++
@@ -1,9 +0,0 @@
-
-// Open t6 folder in your IDE/editor.
-// Make a script that opens an alert window that says 'Button Clicked' when the <button> element is clicked. (1p)
-'use strict';
-
-const button = document.querySelector('button');
-button.addEventListener('click', function(){
-  alert('Button Clicked');
-});
